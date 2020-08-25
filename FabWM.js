e1bceedf80138ce529d5e2f929026f9f5c2b4ef3
faboosh <fabulo1998@gmail.com>
2020-08-25 11:38:01 +0200

    changed CreateClient function

--- FabWM.js
+++ FabWM.js
@@ -7,26 +7,39 @@ function createClient(err, display) {
         let root = display.screen[0].root;
         let wid = X.AllocID();
         X.CreateWindow(
-            wid, root,        // new window id, parent
-            0, 0, 100, 100,   // x, y, w, h
-            0, 0, 0, 0,       // border, depth, class, visual
-            { eventMask: Exposure|PointerMotion } // other parameters
+          wid,
+          root, // new window id, parent
+          0,
+          0,
+          500,
+          500, // x, y, w, h
+          0,
+          0,
+          0,
+          0, // border, depth, class, visual
+          { eventMask: Exposure | PointerMotion } // other parameters
         );
         X.MapWindow(wid);
         let gc = X.AllocID();
         X.CreateGC(gc, wid);
+        let white = display.screen[0].white_pixel;
+        let black = display.screen[0].black_pixel;
+        cidBlack = X.AllocID();
+        cidWhite = X.AllocID();
+        X.CreateGC(cidBlack, wid, { foreground: black, background: white });
+        X.CreateGC(cidWhite, wid, { foreground: white, background: black });
         X.on('event', function(ev) {
-            if (ev.type == 12)
-            {
-                X.PolyText8(wid, gc, 50, 50, ['Hello, Node.JS!']);
-            }
+          if (ev.type == 12) {
+            X.PolyFillRectangle(wid, cidWhite, [0, 0, 500, 500]);
+            X.PolyText8(wid, cidBlack, 50, 50, ['Hello, Node.JS!']);
+          }
         });
         X.on('error', function(e) {
-            console.log(e);
+          console.log(e);
         });
-    } else {
+      } else {
         console.log(err);
-    }
+      }
 }
 
 x11.createClient(createClient);
